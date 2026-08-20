29e0c92865890200a14bd5a465866937b03ccfd1
ariannesystems <arianne.systems@gmail.com>
2026-08-20 09:31:04 -0300

    Seunda modifcacion 02

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const estudiante = "Escribir aquí el nombre";
 console.log(`Entorno preparado para ${estudiante}`);
 console.log("¡Comienza el curso de Node.js!");
+
+console.log('Segunda Modificacion a Git y GitHub');
